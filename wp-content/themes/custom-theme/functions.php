500efdaa89203224acd74c1841056483960adf97
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-09-15 01:41:16 +0600

    WordPress-Content-Files

---
+++ wp-content/themes/custom-theme/functions.php
@@ -0,0 +1,9 @@
+
+<?php
+/*
+*   Theme Related Functions
+*
+*/
+
+// theme title
+add_theme_support('title-tag');
---
+++ wp-content/themes/custom-theme/index.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+    *   This file is part of index file system
+    */
+?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo("UTF-8"); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- to include wp header -->
+    <?php wp_head(); ?>
+</head>
+<body>
+    <?php body_class(); ?>
+
+<!-- to include wp footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
