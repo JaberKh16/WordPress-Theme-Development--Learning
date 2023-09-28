17e7211aeac146bbbcda8f1e5f839c0bce9f2033
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-09-29 04:06:03 +0600

    Wp-Content-Files

--- wp-content/themes/custom-theme/index.php
+++ wp-content/themes/custom-theme/index.php
@@ -8,61 +8,70 @@
 <section id="body-area" class="site-content">
     <div class="container">
         <main class="site-main" id="main">
+              <!-- Hero Section -->
             <section class="hero">Hero</section>
+              <!-- Services Section -->
             <section class="services">Services</section>
+              <!-- Blog Section -->
             <section class="home-blog">
                 <div class="row">
+                    <!-- Blog Column Section -->
                     <div class="col-md-9">
+                        <!-- Post Section -->
                         <div class="post-area">
-                            <?php if (have_posts() != null) : ?>
-                                <?php while (have_posts()) : the_post(); ?>
-                                    <div class="blog-area">
-                                        <!-- Your code to display blog content goes here -->
-                                        <article>
-                                            <h2 class="post-title"><?php the_title(); ?></h2>
-                                            <div class="meta-info">
-                                                <div class="post-time-and-author">
-                                                    <p class="post-time">Posted in: <?php echo get_the_date(); ?></p>
-                                                    <p class="post-author">Posted By: <?php the_author_posts_link(); ?></p>
-                                                    <p class="post-categoyries">Categories: <?php the_category(''); ?></p>
-                                                    <p class="post-tags">Tags: <?php the_tags('', ',')?></p>
-                                                    <p class="post-content"><?php the_content();?></p>
+                            <!-- Blog Items Section -->
+                            <div class="blog-items">
+                                <?php if (have_posts() != null) : ?>
+                                    <?php while (have_posts()) : the_post(); ?>
+                                        <div class="blog-area">
+                                            <!-- Your code to display blog content goes here -->
+                                            <article>
+                                                <h2 class="post-title"><?php the_title(); ?></h2>
+                                                <div class="meta-info">
+                                                    <div class="post-time-and-author">
+                                                        <p class="post-time">Posted in: <?php echo get_the_date(); ?></p>
+                                                        <p class="post-author">Posted By: <?php the_author_posts_link(); ?></p>
+                                                        <p class="post-categoyries">Categories: <?php the_category(''); ?></p>
+                                                        <p class="post-tags">Tags: <?php the_tags('', ',')?></p>
+                                                        <p class="post-content"><?php the_content();?></p>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </article>
-                                        
-                                    </div>
-                                <?php endwhile; ?>
-                            <?php else : ?>
-                                <div class="blog-area">
-                                    <div class="post-thumbnails__area">
-                                        <!-- <?php echo the_post_thumbnail('post-thumbnails'); ?> -->
-                                        <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails');?></a>
-                                    </div>
-                                    <div class="post-details__area">
-                                        <div class="post-title">
-                                            <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                                            </article>
+                                            
                                         </div>
+                                    <?php endwhile; ?>
+                                <?php else : ?>
+                                    <div class="blog-area">
+                                        <div class="post-thumbnails__area">
+                                            <!-- <?php echo the_post_thumbnail('post-thumbnails'); ?> -->
+                                            <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails');?></a>
+                                        </div>
+                                        <div class="post-details__area">
+                                            <div class="post-title">
+                                                <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                                            </div>
+                                        </div>
+                                        <p><?php _e('No post found', 'setup_english'); ?></p>
+                                        <div id="page_nav">
+                                        <?php if ('setup_pagination'): ?>
+                                            <?php  setup_pagination(); ?> 
+                                        <?php else: ?>
+                                            <?php next_posts_link(); ?>
+                                            <?php previous_posts_link(); ?>
+                                        <?php endif; ?>
                                     </div>
-                                    <p><?php _e('No post found', 'setup_english'); ?></p>
-                                    <div id="page_nav">
-                                    <?php if ('setup_pagination'): ?>
-                                        <?php  setup_pagination(); ?> 
-                                    <?php else: ?>
-                                        <?php next_posts_link(); ?>
-                                        <?php previous_posts_link(); ?>
-                                    <?php endif; ?>
-                                </div>
-                                </div>
-                                
-                            <?php endif; ?>
+                                    </div>
+                                    
+                                <?php endif; ?>
+                            </div>
+                            
                         </div>
                     
                     </div>
+                    <!-- Sidebar Column Section -->
                     <div class="col-md-3">
                         <h2>Sidebar Area</h2>
                     </div>
-                    <?php the_content(); ?>
                 </div>
             </section>
         </main>
